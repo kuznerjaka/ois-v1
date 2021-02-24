1fc42a823b2bad502bf0b681be440c1a13ceb1a6
sloncekDumbo <janez2001@gmail.com>
2021-02-24 13:50:33 +0100

    Metoda izpisa ter najlepša slika mene.

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,4 +2,9 @@ public class Gravitacija {
     public static void main(String []args) {
         System.out.print("OIS je zakon!");
     }
+	
+	public static void izpis(double v) {
+		double a = izracun(v);
+		System.out.printf("Nadmorska višina: %.2f m, gravitacijski pospešek: %.2f m/s^2.%n", v, a);
+	}
 }
